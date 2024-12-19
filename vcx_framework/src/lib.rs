c44d448d6dca45f397bcb9ae1c2ef083b6c83055
James Ebert <jamesebert.k@gmail.com>
2024-12-19 15:33:51 -0800

    feat: adjust messaging to a service
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- vcx_framework/src/lib.rs
+++ vcx_framework/src/lib.rs
@@ -22,11 +22,13 @@ pub mod error {
     impl std::error::Error for VCXFrameworkError {}
 }
 
-pub mod messaging_module {
+pub mod messaging_service {
+    use std::sync::Arc;
+
     use thiserror::Error;
 
     use aries_vcx::{
-        aries_vcx_wallet::wallet::{askar::packing_types::Jwe, base_wallet::BaseWallet},
+        aries_vcx_wallet::wallet::base_wallet::BaseWallet,
         did_doc::schema::{service::typed::ServiceType, utils::error::DidDocumentLookupError},
         did_parser_nom::Did,
         did_peer::{
@@ -73,125 +75,152 @@ pub mod messaging_module {
         ConnectionRecordNotFound(Uuid),
     }
 
-    pub async fn send_message(
-        message: &AriesMessage,
-        connection_id: &Uuid,
-        _preferred_transports: Option<&[TransportScheme]>,
-        wallet: &impl BaseWallet,
-        did_resolver_registry: did_resolver_registry::ResolverRegistry,
-        transport_registry: TransportRegistry,
-        connection_repository: ConnectionRepository<
-            impl VCXFrameworkStorage<ConnectionRecordData, ConnectionRecordTagKeys>,
-        >,
-        did_repository: DidRepository<impl VCXFrameworkStorage<DidRecordData, DidRecordTagKeys>>,
-    ) -> Result<(), MessagingError> {
-        info!(
-            "Sending Aries Message to connection `{}`:
-        {:?}",
-            connection_id, message
-        );
-
-        let connection_record: Record<ConnectionRecordData, ConnectionRecordTagKeys> =
-            connection_repository
-                .get_record(connection_id)
-                .map_err(|_| MessagingError::ConnectionRecordNotFound(connection_id.clone()))?
-                .ok_or(MessagingError::ConnectionRecordNotFound(
-                    connection_id.clone(),
-                ))?;
-
-        // TODO Save DIDs in DID Repository (important for finding relevant connection on inbound message)
-
-        send_message_by_did(
-            message,
-            connection_record.data.our_did,
-            connection_record.data.their_did,
-            _preferred_transports,
-            wallet,
-            did_resolver_registry,
-            transport_registry,
-        )
-        .await?;
-        Ok(())
+    pub struct MessagingService<W: BaseWallet> {
+        did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
+        // connection_repository: Arc<
+        //     ConnectionRepository<
+        //         VCXFrameworkStorage<ConnectionRecordData, ConnectionRecordTagKeys>,
+        //     >,
+        // >,
+        // did_repository:
+        //     Arc<DidRepository<impl VCXFrameworkStorage<DidRecordData, DidRecordTagKeys>>>,
+        transport_registry: Arc<TransportRegistry>,
+        wallet: Arc<W>,
     }
 
-    // Should this be restricted to sender_did being a peer did? (probably not)
-    async fn send_message_by_did(
-        message: &AriesMessage,
-        sender_did: PeerDid<Numalgo4>,
-        receiver_did: Did,
-        _preferred_transports: Option<&[TransportScheme]>,
-        wallet: &impl BaseWallet,
-        did_resolver_registry: did_resolver_registry::ResolverRegistry,
-        transport_registry: TransportRegistry,
-    ) -> Result<(), MessagingError> {
-        debug!(
-            "Sending Aries Message {}
-              to Receiver DID {}
-              from Sender DID {}",
-            &message, &receiver_did, &sender_did
-        );
+    impl<W: BaseWallet> MessagingService<W> {
+        pub fn new(
+            did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
+            transport_registry: Arc<TransportRegistry>,
+            wallet: Arc<W>,
+        ) -> Self {
+            Self {
+                did_resolver_registry,
+                transport_registry,
+                wallet,
+            }
+        }
+        pub async fn send_message(
+            &self,
+            message: &AriesMessage,
+            connection_id: &Uuid,
+            _preferred_transports: Option<&[TransportScheme]>,
+            connection_repository: ConnectionRepository<
+                impl VCXFrameworkStorage<ConnectionRecordData, ConnectionRecordTagKeys>,
+            >,
+            did_repository: DidRepository<
+                impl VCXFrameworkStorage<DidRecordData, DidRecordTagKeys>,
+            >,
+        ) -> Result<(), MessagingError> {
+            info!(
+                "Sending Aries Message to connection `{}`:
+            {:?}",
+                connection_id, message
+            );
 
-        let receiver_did_document = did_resolver_registry
-            .resolve(&receiver_did, &Default::default())
-            .await
-            .map_err(|err| MessagingError::DidResolution(err, receiver_did.to_string()))?
-            .did_document;
-        let sender_did_document = sender_did
-            .resolve_did_doc()
-            .map_err(|err| MessagingError::DidResolutionPeerDid(err, sender_did.to_string()))?;
+            let connection_record: Record<ConnectionRecordData, ConnectionRecordTagKeys> =
+                connection_repository
+                    .get_record(connection_id)
+                    .map_err(|_| MessagingError::ConnectionRecordNotFound(connection_id.clone()))?
+                    .ok_or(MessagingError::ConnectionRecordNotFound(
+                        connection_id.clone(),
+                    ))?;
 
-        // TODO: need to provide a way of iterating through all available services, in order of transport preference, instead of just taking the first available service. This would also allow us additional services if one fails.
-        // Allow override of default preferred transport scheme order (as protocols may dictate or prefer specific protocols)
-        // let protocols_to_try = preferred_transports.unwrap_or(PREFERRED_PROTOCOL_ORDER.to_vec());
+            // TODO Save DIDs in DID Repository (important for finding relevant connection on inbound message)
 
-        let receiver_service = receiver_did_document
-            .get_service_of_type(&ServiceType::DIDCommV1)
-            .map_err(|err| MessagingError::InvalidDidDocService(err, receiver_did.to_string()))?;
-
-        let encrypted_message = EncryptionEnvelope::create(
-            wallet,
-            message.to_string().as_bytes(),
-            &sender_did_document,
-            &receiver_did_document,
-            receiver_service.id(),
-        )
-        .await
-        .map_err(MessagingError::EncryptMessage)?;
-
-        trace!(
-            "EncryptedMessage to send: {}",
-            String::from_utf8_lossy(&encrypted_message.0)
-        );
-
-        let returned_message = transport_registry
-            .send_message(
-                encrypted_message,
-                receiver_service.service_endpoint().to_owned(),
+            self.send_message_by_did(
+                message,
+                connection_record.data.our_did,
+                connection_record.data.their_did,
+                _preferred_transports,
             )
-            .await
-            .map_err(MessagingError::OutboundTransportError)?;
+            .await?;
 
-        debug!("Sent message");
-
-        // Handle inbound message if one was returned due to a return route transport decorator (DIDComm v1) or return route extension (DIDComm v2)
-        if returned_message.is_some() {
-            debug!("Handling received message returned via return route mechanism");
-            // TODO: Check whether outbound message contained return route field, if not, we should log error upon receiving message and send problem report if possible
-            // let return_route_enabled = false;
-
-            // TODO
+            info!("Sent Aries Message to connection `{}`", connection_id);
+            Ok(())
         }
 
-        // Event emitting
-        // TODO
-        // self.emit_event(MessagingEvents::OutboundMessage(OutboundMessage {
-        //     message: message.clone(),
-        //     encrypted_message: encrypted_message.clone(),
-        //     sender_did: sender_did.clone(),
-        //     receiver_did: receiver_did.clone(),
-        // }));
+        // Should this be restricted to sender_did being a peer did? (probably not)
+        async fn send_message_by_did(
+            &self,
+            message: &AriesMessage,
+            sender_did: PeerDid<Numalgo4>,
+            receiver_did: Did,
+            _preferred_transports: Option<&[TransportScheme]>,
+        ) -> Result<(), MessagingError> {
+            debug!(
+                "Sending Aries Message {}
+                  to Receiver DID {}
+                  from Sender DID {}",
+                &message, &receiver_did, &sender_did
+            );
 
-        Ok(())
+            let receiver_did_document = self
+                .did_resolver_registry
+                .resolve(&receiver_did, &Default::default())
+                .await
+                .map_err(|err| MessagingError::DidResolution(err, receiver_did.to_string()))?
+                .did_document;
+            let sender_did_document = sender_did
+                .resolve_did_doc()
+                .map_err(|err| MessagingError::DidResolutionPeerDid(err, sender_did.to_string()))?;
+
+            // TODO: need to provide a way of iterating through all available services, in order of transport preference, instead of just taking the first available service. This would also allow us additional services if one fails.
+            // Allow override of default preferred transport scheme order (as protocols may dictate or prefer specific protocols)
+            // let protocols_to_try = preferred_transports.unwrap_or(PREFERRED_PROTOCOL_ORDER.to_vec());
+
+            let receiver_service = receiver_did_document
+                .get_service_of_type(&ServiceType::DIDCommV1)
+                .map_err(|err| {
+                    MessagingError::InvalidDidDocService(err, receiver_did.to_string())
+                })?;
+
+            let encrypted_message = EncryptionEnvelope::create(
+                self.wallet.as_ref(),
+                message.to_string().as_bytes(),
+                &sender_did_document,
+                &receiver_did_document,
+                receiver_service.id(),
+            )
+            .await
+            .map_err(MessagingError::EncryptMessage)?;
+
+            trace!(
+                "EncryptedMessage to send: {}",
+                String::from_utf8_lossy(&encrypted_message.0)
+            );
+
+            let returned_message = self
+                .transport_registry
+                .send_message(
+                    encrypted_message,
+                    receiver_service.service_endpoint().to_owned(),
+                )
+                .await
+                .map_err(MessagingError::OutboundTransportError)?;
+
+            debug!("Sent message");
+
+            // Handle inbound message if one was returned due to a return route transport decorator (DIDComm v1) or return route extension (DIDComm v2)
+            if returned_message.is_some() {
+                debug!("Handling received message returned via return route mechanism");
+                // TODO: Check whether outbound message contained return route field, if not, we should log error upon receiving message and send problem report if possible
+                // let return_route_enabled = false;
+
+                // TODO
+            }
+
+            // Event emitting
+            // TODO
+            // self.emit_event(MessagingEvents::OutboundMessage(OutboundMessage {
+            //     message: message.clone(),
+            //     encrypted_message: encrypted_message.clone(),
+            //     sender_did: sender_did.clone(),
+            //     receiver_did: receiver_did.clone(),
+            // }));
+
+            Ok(())
+        }
     }
 
     #[cfg(test)]
@@ -216,8 +245,7 @@ pub mod messaging_module {
         use url::Url;
 
         use crate::{
-            repositories::connection_repository, storage::in_memory_storage::InMemoryStorage,
-            test_init, transport::HttpTransport,
+            storage::in_memory_storage::InMemoryStorage, test_init, transport::HttpTransport,
         };
 
         use super::*;
@@ -293,18 +321,21 @@ pub mod messaging_module {
             let in_memory_storage_dids = InMemoryStorage::<DidRecordData, DidRecordTagKeys>::new();
             let mut did_repository = DidRepository::new(in_memory_storage_dids);
 
-            send_message(
-                &message,
-                &connection_id,
-                Some(&[TransportScheme::HTTP, TransportScheme::WS]),
-                &wallet,
-                did_resolver_registry,
-                transport_registry,
-                connection_repository,
-                did_repository,
-            )
-            .await
-            .unwrap()
+            let messaging_service = MessagingService::new(
+                Arc::new(did_resolver_registry),
+                Arc::new(transport_registry),
+                Arc::new(wallet),
+            );
+            messaging_service
+                .send_message(
+                    &message,
+                    &connection_id,
+                    Some(&[TransportScheme::HTTP, TransportScheme::WS]),
+                    connection_repository,
+                    did_repository,
+                )
+                .await
+                .unwrap()
         }
     }
 }
