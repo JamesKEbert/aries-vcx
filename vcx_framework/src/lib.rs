3d54393082d3aeba7445a6f5f0768360d94b50fd
James Ebert <jamesebert.k@gmail.com>
2025-02-11 07:47:10 -0800

    feat: add base connectionservice, errors for it
    Signed-off-by: James Ebert <jamesebert.k@gmail.com>

--- vcx_framework/src/lib.rs
+++ vcx_framework/src/lib.rs
@@ -23,32 +23,59 @@ pub mod error {
 }
 
 pub mod connection_service {
-    use std::sync::Arc;
+    use std::{collections::HashMap, sync::Arc};
 
-    use aries_vcx::aries_vcx_wallet::wallet::base_wallet::BaseWallet;
+    use aries_vcx::{
+        aries_vcx_wallet::wallet::base_wallet::BaseWallet,
+        errors::error::AriesVcxError,
+        handlers::out_of_band::receiver::OutOfBandReceiver,
+        messages::decorators::transport::{ReturnRoute, Transport},
+        protocols::did_exchange::state_machine::{
+            generic::GenericDidExchange,
+            helpers::create_peer_did_4,
+            requester::helpers::{
+                invitation_get_acceptable_did_exchange_version, invitation_get_first_did_service,
+            },
+        },
+    };
     use thiserror::Error;
+    use url::Url;
+    use uuid::Uuid;
 
     use crate::{
+        messaging_service::{MessagingError, MessagingService},
         repositories::{
             connection_repository::{
                 ConnectionRecordData, ConnectionRecordTagKeys, ConnectionRepository,
             },
             did_repository::{DidRecordData, DidRecordTagKeys, DidRepository},
         },
-        storage::base::VCXFrameworkStorage,
+        storage::{base::VCXFrameworkStorage, record::Record},
+        transport::TransportScheme,
     };
 
     #[derive(Error, Debug)]
     pub enum ConnectionServiceError {
-        // #[error("invalid transport scheme `{0}`")]
-        // InvalidTransportScheme(#[source] TransportError, String),
+        #[error("Unsupporrted Did Exchange Version in Invitation, unable to create connection")]
+        InvalidDidExchangeVersion(#[source] AriesVcxError),
+        #[error("Error with peer DID")]
+        PeerDIDError(#[source] AriesVcxError),
+        #[error("Error finding DID service in Invitation")]
+        NoDIDServiceFound(#[source] AriesVcxError),
+        #[error("Unable to create connection request")]
+        ErrorCreateConnectionRequest(#[source] AriesVcxError),
+        #[error("Error Sending Message")]
+        ErrorSendingMessage(#[source] MessagingError),
     }
 
     pub struct ConnectionService<W: BaseWallet> {
         did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
         connection_repository: Arc<ConnectionRepository>,
         did_repository: Arc<DidRepository>,
+        messaging_service: Arc<MessagingService<W>>,
         wallet: Arc<W>,
+        agent_endpoint: Url,
+        agent_label: String,
     }
 
     impl<W: BaseWallet> ConnectionService<W> {
@@ -56,14 +83,95 @@ pub mod connection_service {
             did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
             connection_repository: Arc<ConnectionRepository>,
             did_repository: Arc<DidRepository>,
+            messaging_service: Arc<MessagingService<W>>,
             wallet: Arc<W>,
+            agent_endpoint: Url,
+            agent_label: String,
         ) -> Self {
             Self {
                 did_resolver_registry,
                 connection_repository,
                 did_repository,
+                messaging_service,
                 wallet,
+                agent_endpoint,
+                agent_label,
             }
+        }
+
+        pub async fn connect(
+            &self,
+            invitation: OutOfBandReceiver,
+            mediated: bool,
+            specific_mediator_id: Option<Uuid>,
+        ) -> Result<(), ConnectionServiceError> {
+            debug!(
+                "Requesting Connection via DID Exchange with invitation {}",
+                invitation
+            );
+
+            // TODO - peer did we create here should be able to be mediated (routing keys should be provided or generated)
+            // TODO - create_peer_did_4() function should move into peer did 4 implementation
+            let (peer_did, _our_verkey) =
+                create_peer_did_4(self.wallet.as_ref(), self.agent_endpoint.clone(), vec![])
+                    .await
+                    .map_err(ConnectionServiceError::PeerDIDError)?;
+
+            // Get Inviter DID from invitation
+            let inviter_did = invitation_get_first_did_service(&invitation.oob)
+                .map_err(ConnectionServiceError::NoDIDServiceFound)?;
+
+            // Get DID Exchange version to use based off of invitation handshake protocols
+            let version = invitation_get_acceptable_did_exchange_version(&invitation.oob)
+                .map_err(ConnectionServiceError::InvalidDidExchangeVersion)?;
+
+            // If not mediated, we will specify the transport decorator with return route all to allow for the DID Exchange response message to be returned immediately. Most useful in mobile contexts for establishing connections with mediators
+            let transport_decorator =
+                (!mediated).then_some(Transport::builder().return_route(ReturnRoute::All).build());
+
+            // TODO - Fix DID Exchange Goal Code definition - Should not be "To establish a connection" - rather should be a goal code or not specified (IIRC)
+            let (state_machine, request) = GenericDidExchange::construct_request(
+                &self.did_resolver_registry,
+                Some(invitation.oob.id.clone()),
+                &inviter_did,
+                &peer_did,
+                self.agent_label.to_owned(),
+                version,
+                transport_decorator,
+            )
+            .await
+            .map_err(ConnectionServiceError::ErrorCreateConnectionRequest)?;
+
+            trace!("Created DID Exchange State Machine and request message, going to send message");
+
+            let connection_id = Uuid::new_v4();
+
+            self.messaging_service
+                .send_message(
+                    &request.into(),
+                    connection_id,
+                    Some(&[TransportScheme::HTTP, TransportScheme::WS]),
+                )
+                .await
+                .map_err(ConnectionServiceError::ErrorSendingMessage)?;
+
+            let mut record_keys = HashMap::new();
+            record_keys.insert(ConnectionRecordTagKeys::OurDid, peer_did.to_string());
+            record_keys.insert(ConnectionRecordTagKeys::TheirDid, inviter_did.to_string());
+            let record = Record::new(
+                connection_id.to_string(),
+                ConnectionRecordData {
+                    our_did: peer_did,
+                    their_did: inviter_did,
+                },
+                Some(record_keys),
+            );
+
+            self.connection_repository.add_or_update_record(record);
+
+            //TODO - Emit Event
+
+            Ok(())
         }
     }
 }
@@ -123,13 +231,8 @@ pub mod messaging_service {
 
     pub struct MessagingService<W: BaseWallet> {
         did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
-        // connection_repository: Arc<
-        //     ConnectionRepository<
-        //         VCXFrameworkStorage<ConnectionRecordData, ConnectionRecordTagKeys>,
-        //     >,
-        // >,
-        // did_repository:
-        //     Arc<DidRepository<impl VCXFrameworkStorage<DidRecordData, DidRecordTagKeys>>>,
+        connection_repository: Arc<ConnectionRepository>,
+        did_repository: Arc<DidRepository>,
         transport_registry: Arc<TransportRegistry>,
         wallet: Arc<W>,
     }
@@ -138,21 +241,23 @@ pub mod messaging_service {
         pub fn new(
             did_resolver_registry: Arc<did_resolver_registry::ResolverRegistry>,
             transport_registry: Arc<TransportRegistry>,
+            connection_repository: Arc<ConnectionRepository>,
+            did_repository: Arc<DidRepository>,
             wallet: Arc<W>,
         ) -> Self {
             Self {
                 did_resolver_registry,
                 transport_registry,
+                connection_repository,
+                did_repository,
                 wallet,
             }
         }
         pub async fn send_message(
             &self,
             message: &AriesMessage,
-            connection_id: &Uuid,
+            connection_id: Uuid,
             _preferred_transports: Option<&[TransportScheme]>,
-            connection_repository: ConnectionRepository,
-            did_repository: DidRepository,
         ) -> Result<(), MessagingError> {
             info!(
                 "Sending Aries Message to connection `{}`:
@@ -160,18 +265,16 @@ pub mod messaging_service {
                 connection_id, message
             );
 
-            let connection_record: Record<ConnectionRecordData, ConnectionRecordTagKeys> =
-                connection_repository
-                    .get_record(connection_id)
-                    .map_err(|_| MessagingError::ConnectionRecordNotFound(connection_id.clone()))?
-                    .ok_or(MessagingError::ConnectionRecordNotFound(
-                        connection_id.clone(),
-                    ))?;
+            let connection_record: Record<ConnectionRecordData, ConnectionRecordTagKeys> = self
+                .connection_repository
+                .get_record(&connection_id)
+                .map_err(|_| MessagingError::ConnectionRecordNotFound(connection_id))?
+                .ok_or(MessagingError::ConnectionRecordNotFound(connection_id))?;
 
             // TODO Save DIDs in DID Repository (important for finding relevant connection on inbound message)
             // Actually -- we should check for DID on connection_record -- it should be set at connection record creation
 
-            self.send_message_by_did(
+            self.send_message_to_did(
                 message,
                 connection_record.data.our_did,
                 connection_record.data.their_did,
@@ -184,7 +287,7 @@ pub mod messaging_service {
         }
 
         // Should this be restricted to sender_did being a peer did? (probably not)
-        async fn send_message_by_did(
+        async fn send_message_to_did(
             &self,
             message: &AriesMessage,
             sender_did: PeerDid<Numalgo4>,
@@ -367,15 +470,15 @@ pub mod messaging_service {
             let messaging_service = MessagingService::new(
                 Arc::new(did_resolver_registry),
                 Arc::new(transport_registry),
+                Arc::new(connection_repository),
+                Arc::new(did_repository),
                 Arc::new(wallet),
             );
             messaging_service
                 .send_message(
                     &message,
-                    &connection_id,
+                    connection_id,
                     Some(&[TransportScheme::HTTP, TransportScheme::WS]),
-                    connection_repository,
-                    did_repository,
                 )
                 .await
                 .unwrap()
